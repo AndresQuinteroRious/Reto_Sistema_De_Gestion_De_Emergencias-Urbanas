2f2317acc9d78c4ffb5d05062bbb52f83a21f339
AndresQuinteroRiuos <quintero2273@hotmail.com>
2025-05-20 22:13:14 -0500

    Add files via upload

---
+++ Sistema De Gestion De Emergencias Urbanas/src/Model/interface/iservicioEmergencia.java
@@ -0,0 +1,12 @@
+
+
+public interface iservicioEmergencia {
+    String getId();
+    int getCombustible(); 
+    boolean estaDisponible();
+    int getPersonalDisponible();
+    void asignarPersonal(int cantidad);
+    void asignarCombustible(double cantidad);
+    void cargarCombustible(double cantidad);
+   
+}
